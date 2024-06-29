fc12dc396360040a39c3ba7aea4249ddacb0a9b1
ArnabTo <arnabs929@gmail.com>
2024-06-29 18:52:43 +0600

    added services

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Landing from "@/components/Hero/Landing";
 import NavBar from "@/components/Nav/NavBar";
+import Service from "@/components/Services/Service";
 
 export default function Home() {
   return (
     <main>
       <NavBar/>
       <Landing/>
+      <Service/>
     </main>
   );
 }
